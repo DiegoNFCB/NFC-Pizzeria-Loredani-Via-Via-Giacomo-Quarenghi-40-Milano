05c41ad088e9bbade93efbb8f25c7d6e833516d5
DiegoNFCB <cc2608910@gmail.com>
2026-08-13 14:11:47 +0200

    Update parteLogicaSito2.js

--- parteLogicaSito2.js
+++ parteLogicaSito2.js
@@ -1,7 +1,7 @@
 const reviewBtn = document.getElementById("reviewBtn");
 
 // IL LINK NON E' CORRETTO ASPETTO QUELLO DEL NEGOZIO 
-const googleReviewUrl = "https://search.google.com/local/writereview?placeid=ChIJ2wXtEufBhkcRP0peqnaEGpM&utm_source=chatgpt.com";
+const googleReviewUrl = "https://search.google.com/local/writereview?placeid=ChIJyQ3sNjXAhkcRFK3_BbuL95Q&utm_source=chatgpt.com";
 
 
 reviewBtn.addEventListener("click", function () {
