e6feca6d6417926e393f951b37005bd4a47c4c3f
DiegoNFCB <cc2608910@gmail.com>
2026-08-13 14:38:33 +0200

    Update parteLogicaSito2.js

--- parteLogicaSito2.js
+++ parteLogicaSito2.js
@@ -1,7 +1,7 @@
 const reviewBtn = document.getElementById("reviewBtn");
 
 // IL LINK NON E' CORRETTO ASPETTO QUELLO DEL NEGOZIO 
-const googleReviewUrl = "https://www.google.com/search?hl=it-IT&gl=it&q=Ristorante+Pizzeria+Bar+Al+Bistro%27,+Via+delle+Ande,+16,+20151+Milano+MI&ludocid=10599930318169197119&lsig=AB86z5VbZ6CPQyMhgPZ2NLFhsaPO&utm_source=chatgpt.com#lrd=0x4786c1e712ed05db:0x931a8476aa5e4a3f,3";
+const googleReviewUrl = "https://g.page/r/ChIJH9zq4YqfhkcRj8h0m9t1u9E/review";
 
 
 reviewBtn.addEventListener("click", function () {
