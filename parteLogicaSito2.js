fd4cefb50d77fadca275bc0d67105a59e281501c
DiegoNFCB <cc2608910@gmail.com>
2026-08-13 11:47:50 +0200

    Update parteLogicaSito2.js
    tutto A posto manca SOLO il link che mi darà la pizzeria

--- parteLogicaSito2.js
+++ parteLogicaSito2.js
@@ -1,5 +1,6 @@
 const reviewBtn = document.getElementById("reviewBtn");
 
+// IL LINK NON E' CORRETTO ASPETTO QUELLO DEL NEGOZIO 
 const googleReviewUrl = "https://g.page/r/ChIJr8zq4YqfhkcRj8h0m9t1u9E/review";
 
 
