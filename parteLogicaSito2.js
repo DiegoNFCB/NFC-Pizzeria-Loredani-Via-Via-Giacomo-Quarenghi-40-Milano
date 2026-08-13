3487f2943c598df8bc2c2ac4ff0459a1dca4c6bc
DiegoNFCB <cc2608910@gmail.com>
2026-08-13 09:49:27 +0200

    Update parteLogicaSito2.js

--- parteLogicaSito2.js
+++ parteLogicaSito2.js
@@ -2,7 +2,7 @@
 const reviewBtn = document.getElementById("reviewBtn");
 
 // URL ufficiale per lasciare una recensione Google
-const googleReviewUrl = "https://search.google.com/local/writereview?placeid=ChIJu8oaCQLBhkcRRbsbzvuReCA";
+const googleReviewUrl = https: "//search.google.com/local/writereview?placeid=ChIJr8zq4YqfhkcRj8h0m9t1u9E";
 
 // Evento click sul pulsante
 reviewBtn.addEventListener("click", function () {
